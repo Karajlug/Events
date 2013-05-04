eac4cd7720a2dfd2d62d2082121d9c494e58ee5f
Sameer Rahmani <lxsameer@gnu.org>
2013-05-05 03:19:46 +0430

    very basic index page, done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,12 @@
 class UserController < ApplicationController
+
+  # Reponse to /
+  def index
+    require 'date'
+
+    @user = User.new
+    @remaining = (Date.new(2013, 10, 21) - Date.today).to_i
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SFD2013::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'user#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -46,4 +46,5 @@ SFD2013::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
