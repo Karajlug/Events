c8dca3c0300877e002d6ebb78d62d7a81fcf8830
Sameer Rahmani <lxsameer@gnu.org>
2013-07-24 01:35:12 +0430

    Copyright notice added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_action :set_local_for_admin
-
-  def set_local_for_admin
-    if self.class == Admin::DashboardController
-      puts ">>>>>>>>>>>>>>"
-      I18n.locale = :en
-    end
-  end
 end
