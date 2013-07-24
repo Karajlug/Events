0d51cb4465d2b8b5232252f392777e77ad9d7d16
Sameer Rahmani <lxsameer@gnu.org>
2013-07-24 16:03:43 +0430

    active admin strong parameters bug fixed

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -17,4 +17,11 @@ ActiveAdmin.register AdminUser do
     end
     f.actions
   end
+
+  controller do
+    def permitted_params
+      params.permit(:admin_user => [:email, :password, :password_confirmation])
+    end
+  end
+
 end
