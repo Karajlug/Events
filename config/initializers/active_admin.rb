1441cebad877d5256338dc96ab87239d35202f5a
Sameer Rahmani <lxsameer@gnu.org>
2014-01-15 12:49:07 +0330

    couple of bug fixed

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ActiveAdmin.setup do |config|
 
   # == Site Title
@@ -5,7 +6,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Sfd2013"
+  config.site_title = "Karajlug Events"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
